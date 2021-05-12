560a2dfb481b3184d7c33b3a32f53ea6b7549dd3
FERREIRAASGARUK <marcusviniciusmatarazzo@gmail.com>
2021-05-12 10:51:37 -0300

    aplicando responsividade

--- src/App.js
+++ src/App.js
@@ -1,44 +1,46 @@
-import './App.css';
-import { Formik, Field, Form, ErrorMessage } from 'formik';
-import React, { useEffect, useState } from 'react';
+import "./App.css";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
 // import * as Yup from 'yup';
 
-
 function App() {
-  const [campos, setCampos] = useState()
-
+  const [campos, setCampos] = useState();
 
   function fillFields(props) {
-    console.log(props)
+    console.log(props);
   }
-
 
   return (
     <div className="App">
       <h1 className="title">CONSULTAR-CEP</h1>
 
-
       <Formik
-        initialValues={{ Cep: '', Logradouro: '', Bairro: '', Cidade: '', Estado: '' }}
-
-        onSubmit={values => {
-
+        initialValues={{
+          Cep: "",
+          Logradouro: "",
+          Bairro: "",
+          Cidade: "",
+          Estado: "",
+        }}
+        onSubmit={(values) => {
           fetch(`https://viacep.com.br/ws/${values.Cep}/json`, {
-            method: 'GET',
-            mode: 'cors',
-            cache: 'default'
-          }).then(response => { response.json().then(data => { setCampos(data) }) })
+            method: "GET",
+            mode: "cors",
+            cache: "default",
+          }).then((response) => {
+            response.json().then((data) => {
+              setCampos(data);
+            });
+          });
 
-          values.Logradouro = campos.logradouro
-          values.Bairro = campos.bairro
-          values.Cidade = campos.localidade
-          values.Estado = campos.uf
-
+          values.Logradouro = campos.logradouro;
+          values.Bairro = campos.bairro;
+          values.Cidade = campos.localidade;
+          values.Estado = campos.uf;
         }}
       >
         <Form className="formulario">
-
-
           <label htmlFor="Cep">Cep</label>
           <Field className="campo" name="Cep" type="text" />
           <ErrorMessage className="a" name="Cep" />
@@ -59,11 +61,19 @@ function App() {
           <Field className="campo" name="Estado" type="text" />
           <ErrorMessage name="Estado" />
 
-          <button className="botao" onClick={() => { fillFields(campos) }} type="submit">Enviar</button>
+          <Button
+            className="botao"
+            variant="outlined"
+            color="primary"
+            onClick={() => {
+              fillFields(campos);
+            }}
+            type="submit"
+          >
+            Enviar
+          </Button>
         </Form>
       </Formik>
-
-
     </div>
   );
 }
