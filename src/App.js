eed2552dfebf3b11b055f08aeb1cfca9d49dbe2d
FERREIRAASGARUK <marcusviniciusmatarazzo@gmail.com>
2021-05-12 20:44:45 -0300

    projeto completo

--- src/App.js
+++ src/App.js
@@ -1,79 +1,140 @@
 import "./App.css";
 import { Formik, Field, Form, ErrorMessage } from "formik";
-import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
+import React, { useState, useEffect } from "react";
+import { Container, Resultado, Title, Title2, Line1, Line2 } from "./style";
+
 // import * as Yup from 'yup';
 
 function App() {
-  const [campos, setCampos] = useState();
+  const [update, setUpdate] = useState(false);
+  const [campos, setCampos] = useState({
+    bairro: "",
+    cep: "",
+    complemento: "",
+    ddd: "",
+    gia: "",
+    ibge: "",
+    localidade: "",
+    logradouro: "",
+    siafi: "",
+    uf: "",
+  });
 
-  function fillFields(props) {
-    console.log(props);
-  }
+  useEffect(() => {
+    setUpdate(true);
+  }, [campos.cep]);
 
   return (
-    <div className="App">
-      <h1 className="title">CONSULTAR-CEP</h1>
-
-      <Formik
-        initialValues={{
-          Cep: "",
-          Logradouro: "",
-          Bairro: "",
-          Cidade: "",
-          Estado: "",
-        }}
-        onSubmit={(values) => {
-          fetch(`https://viacep.com.br/ws/${values.Cep}/json`, {
-            method: "GET",
-            mode: "cors",
-            cache: "default",
-          }).then((response) => {
-            response.json().then((data) => {
-              setCampos(data);
-            });
-          });
-
-          values.Logradouro = campos.logradouro;
-          values.Bairro = campos.bairro;
-          values.Cidade = campos.localidade;
-          values.Estado = campos.uf;
-        }}
-      >
-        <Form className="formulario">
-          <label htmlFor="Cep">Cep</label>
-          <Field className="campo" name="Cep" type="text" />
-          <ErrorMessage className="a" name="Cep" />
-
-          <label htmlFor="Logradouro">Logradouro</label>
-          <Field className="logradouro" name="Logradouro" type="text" />
-          <ErrorMessage name="Logradouro" />
-
-          <label htmlFor="Bairro">Bairro</label>
-          <Field className="Bairro" name="Bairro" type="text" />
-          <ErrorMessage name="Bairro" />
-
-          <label htmlFor="Cidade">Cidade</label>
-          <Field className="city" name="Cidade" type="text" />
-          <ErrorMessage name="Cidade" />
-
-          <label htmlFor="Estado">Estado</label>
-          <Field className="campo" name="Estado" type="text" />
-          <ErrorMessage name="Estado" />
-
-          <Button
-            className="botao"
-            variant="outlined"
-            color="primary"
-            onClick={() => {
-              fillFields(campos);
+    <div>
+      <h1 className="title">CONSULTAR CEP</h1>
+      <Container>
+        {!update ? (
+          <Formik
+            initialValues={{
+              Cep: "",
+              Logradouro: "",
+              Bairro: "",
+              Cidade: "",
+              Estado: "",
             }}
-            type="submit"
+            onSubmit={(values) => {
+              fetch(`https://viacep.com.br/ws/${values.Cep}/json`, {
+                method: "GET",
+                mode: "cors",
+                cache: "default",
+              }).then((response) => {
+                response.json().then((data) => {
+                  setCampos(data);
+                });
+              });
+
+              values.Logradouro = campos.logradouro;
+              values.Bairro = campos.bairro;
+              values.Cidade = campos.localidade;
+              values.Estado = campos.uf;
+            }}
           >
-            Enviar
-          </Button>
-        </Form>
-      </Formik>
+            <Form className="formulario">
+              <label htmlFor="Cep">Cep</label>
+              <Field className="campo" name="Cep" type="text" />
+              <ErrorMessage className="a" name="Cep" />
+
+              <Button
+                className="botao"
+                variant="contained"
+                color="primary"
+                type="submit"
+              >
+                Buscar
+              </Button>
+            </Form>
+          </Formik>
+        ) : (
+          <div>
+            <Formik
+              initialValues={{
+                Cep: "",
+                Logradouro: "",
+                Bairro: "",
+                Cidade: "",
+                Estado: "",
+              }}
+              onSubmit={(values) => {
+                fetch(`https://viacep.com.br/ws/${values.Cep}/json`, {
+                  method: "GET",
+                  mode: "cors",
+                  cache: "default",
+                }).then((response) => {
+                  response.json().then((data) => {
+                    setCampos(data);
+                  });
+                });
+                console.log(campos);
+                values.Logradouro = campos.logradouro;
+                values.Bairro = campos.bairro;
+                values.Cidade = campos.localidade;
+                values.Estado = campos.uf;
+              }}
+            >
+              <Form className="formulario">
+                <label htmlFor="Cep">Cep</label>
+                <Field className="campo" name="Cep" type="text" />
+                <ErrorMessage className="a" name="Cep" />
+
+                <Button
+                  className="botao"
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                >
+                  Buscar
+                </Button>
+              </Form>
+            </Formik>
+            <Resultado>
+              <Line1>
+                <Title>CEP</Title>
+                <Title>BAIRRO</Title>
+                <Title>LOCALIDADE</Title>
+                <Title>LOGRADOURO</Title>
+                <Title>UF</Title>
+                <Title>DDD</Title>
+              </Line1>
+              <Line2>
+                <Title2 style={{ marginLeft: 20 }}>{campos.cep}</Title2>
+                <Title2>{campos.bairro}</Title2>
+                <Title2>{campos.localidade}</Title2>
+                <Title2>{campos.logradouro}</Title2>
+                <Title2 style={{ marginRight: 0 }} i>
+                  {campos.uf}
+                </Title2>
+                <Title2>{campos.ddd}</Title2>
+              </Line2>
+            </Resultado>
+          </div>
+        )}
+      </Container>
     </div>
   );
 }
---
+++ src/style.js
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 1920px;
+  height: 1080px;
+  background-color: white;
+  border-top: 1px solid black;
+  padding-top: 100px;
+`;
+
+export const Resultado = styled.div`
+  margin-top: 100px;
+  margin-left: 100px;
+  margin-right: 100px;
+  background-color: blue;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+export const Title = styled.div`
+  color: black;
+  font-weight: 800;
+  font-family: Arial;
+  font-size: 30px;
+  margin-left: 50px;
+  background-color: ligth-blue;
+`;
+export const Title2 = styled.div`
+  color: grey;
+  font-weight: 300;
+  font-family: Arial;
+  font-size: 20px;
+  margin-right: 15px;
+`;
+
+export const Line1 = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-directtion: row;
+`;
+export const Line2 = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-directtion: row;
+  background-color: white;
+`;
